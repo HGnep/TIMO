6082572b5fff4cf7e190f5c589450aa0bda65e66
Brian Aarts <bmtc.aarts@gmail.com>
2018-06-20 10:42:42 +0200

    Upgrade DB-setup to .NET Core 2.x

--- Startup.cs
+++ Startup.cs
@@ -4,34 +4,33 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using TIMO.Services;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using TIMO.data;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
-namespace TIMO {
-	public class Startup {
-		public Startup(IHostingEnvironment env) {
-			var builder = new ConfigurationBuilder()
-				.SetBasePath(env.ContentRootPath)
-				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-				.AddEnvironmentVariables();
-
-			Configuration = builder.Build();
+namespace TIMO 
+{
+	public class Startup 
+	{
+		public Startup(IConfiguration configuration) 
+		{
+			Configuration = configuration;
 		}
 
-		public IConfigurationRoot Configuration { get; }
+		public IConfiguration Configuration { get; }
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services) {
+		public void ConfigureServices(IServiceCollection services) 
+		{
 			services.AddMvc();
-			services.AddEntityFramework().AddEntityFrameworkSqlite().AddDbContext<TimoDbContext>(options => options.UseSqlite("TIMO.db"));
+			services.AddEntityFrameworkSqlite().AddDbContext<TimoDbContext>(options => options.UseSqlite("Timo.db"));
 			services.AddScoped<IRelatieService, RelatieService>();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) {
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory) 
+		{
 			loggerFactory.AddConsole();
 
 			app.UseDeveloperExceptionPage();
