1c1ba8297b010de302fd1c7a273ff7a9f36964a1
Brian Aarts <bmtc.aarts@gmail.com>
2018-06-20 10:42:29 +0200

    Upgrade program startup to .NET Core 2.x

--- Program.cs
+++ Program.cs
@@ -1,59 +1,30 @@
 ﻿//using System;
 using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 /*
 using TIMO.Controllers;
 using TIMO.data;
 using TIMO.Services;
 */
 
-namespace TIMO {
-	public class Program {
-		public static void Main(string[] args) {
-
-		var host = new WebHostBuilder()
-			.UseKestrel()
-			.UseContentRoot(Directory.GetCurrentDirectory())
-			.UseIISIntegration()
-			.UseStartup<Startup>()
-			.Build();
-			host.Run();
-		/*
-			using (var db = new TimoDbContext()) {
-				var relatieService = new RelatieService(db);
-				var relatieController = new RelatieController(db, relatieService);
-				var running = true;
-
-				while (running) {
-					Console.WriteLine();
-					Console.WriteLine("Wat wil je doen?");
-					Console.WriteLine("Keuzes: ");
-					Console.WriteLine("1. Tabel weergeven");
-					Console.WriteLine("2. Nieuwe relatie toevoegen");
-					Console.WriteLine("3. Relatie verwijderen");
-					Console.WriteLine("4. Sluit programma");
-
-					Console.WriteLine("Keuze: ");
-					var keuze = int.Parse(Console.ReadLine());
-
-					if (keuze == 1) {
-						relatieController.ReadTable();
-					}
-					else if (keuze == 2) {
-						relatieController.Create();
-					}
-					else if (keuze == 3) {
-						relatieController.Delete();
-					}
-					else if (keuze == 4) {
-						running = false;
-					}
-					else {
-						Console.WriteLine("Dit is geen geldige keuze.");
-					}
-				}
-			}
-		*/
+namespace TIMO
+{
+	public class Program
+	{
+		public static void Main (string[] args)
+		{
+			BuildWebHost(args).Run();
 		}
+
+		public static IWebHost BuildWebHost(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.ConfigureAppConfiguration((hostContext, config) => {
+					config.Sources.Clear();
+					config.AddJsonFile("appsettings.json", optional: true);
+				})
+				.Build();
 	}
 }
